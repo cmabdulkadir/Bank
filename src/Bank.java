d1c3f5673e6a2a5dfbd49091f179ebcae8ef5162
Chaltu Abdulkadir <chaltuabdulkadir@Chaltus-MacBook-Air.local>
2022-11-12 21:47:38 -0600

    created a showOption menu

--- src/Bank.java
+++ src/Bank.java
@@ -12,4 +12,12 @@ public class Bank {
         this.transactions = new HashMap<Integer, Transaction>();
     }
 
+    public void showOption() {
+        System.out.println("\nType (1) to create a new Transaction");
+        System.out.println("Type (2) to read all Transaction");
+        System.out.println("Type (3) to update a Transaction");
+        System.out.println("Type (4) to delete a Transaction ");
+        System.out.println("Type (5) to Exit");
+    }
+
 }
