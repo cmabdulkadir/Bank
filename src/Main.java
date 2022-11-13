b2866fde7dcfb6cf36ad2b4663584f9a443a312c
Shukri Elmi <shukrielmi@Shukris-MacBook-Air.local>
2022-11-13 07:26:11 -0800

    added main method

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,42 @@
+import java.util.Scanner;
 public class Main {
+    public static void main(String[] args) {
+        Bank bank = new Bank();
+
+        Scanner scan = new Scanner(System.in);
+
+        int option = -1;
+
+
+        do {
+            bank.showOption();
+            option = scan.nextInt();
+            switch (option) {
+                case 1:
+                    //create new product
+                    bank.createTransactions();
+                    break;
+                case 2:
+                    //Print all product
+                    bank.readTransactions();
+                    break;
+
+                case 3:
+                    //update a product
+                    bank.updateTransaction();
+                    break;
+                case 4:
+                    //remove a product by name
+                    bank.deleteTransaction();
+                    break;
+                case 5:
+                    //Exit
+                    System.exit(1);
+                    break;
+            }
+
+        } while (option != 0);
+
+    }
+
 }
